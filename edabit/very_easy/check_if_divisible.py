aec5ecefd4d68e8092fb7c8aa9e4b7447544dd7b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-23 21:14:17 -0500

    :sparkles: Update

---
+++ edabit/very_easy/check_if_divisible.py
@@ -0,0 +1,6 @@
+# Return True if an integer is evenly divisible by 5, and False otherwise
+
+def divisible_by_five(number):
+    return True if number % 5 == 0 else False
+
+print(divisible_by_five(4456))
