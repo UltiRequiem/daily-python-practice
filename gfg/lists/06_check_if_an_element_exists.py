9d87bf0a92fb2f5d954668995dafea96a1a36968
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-07 08:12:50 -0500

    :art: Update

---
+++ gfg/lists/06_check_if_an_element_exists.py
@@ -0,0 +1,6 @@
+test_list = [1, 6, 3, 5, 3, 4]
+element_to_find = 5
+
+if __name__ == "__main__":
+    if element_to_find in test_list:
+        print(f"{element_to_find} exists in the the test list.")
