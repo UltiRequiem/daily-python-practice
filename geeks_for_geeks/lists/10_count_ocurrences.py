7940719c38f3bdfe052669026dc7333882f8d24c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-12 17:09:49 -0500

    :art: Update

--- geeks_for_geeks/lists/10_count_ocurrences.py
+++ geeks_for_geeks/lists/10_count_ocurrences.py
@@ -1,4 +1,4 @@
 lst = [8, 6, 8, 10, 8, 20, 10, 8, 8]
 x = 8
 
-print(f'{x} has occurred {lst.count(x)} times.')
+print(f"{x} has occurred {lst.count(x)} times.")
---
+++ geeks_for_geeks/lists/11_sum_and_average.py
@@ -0,0 +1,14 @@
+NUMBER_LIST = [
+    4,
+    5,
+    8,
+    4,
+]
+
+
+count = sum(NUMBER_LIST)
+
+avg = round(count / len(NUMBER_LIST), 2)
+
+print("Sum of all items in the list:", count)
+print("Average of all items in the list:", avg)
