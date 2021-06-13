bf1df0256613ed75b27b1a27f712711dd1a9fd05
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-13 10:24:00 -0500

    :art: Update
    Nice

---
+++ geeks_for_geeks/lists/12_sum_of_number_digits.py
@@ -0,0 +1,7 @@
+NUMBER_LIST = [12, 67, 98, 34]
+  
+print("The original list is : " + str(NUMBER_LIST))
+  
+result = list(map(lambda i: sum(int(char) for char in str(i)), NUMBER_LIST))
+      
+print ("List Integer Summation : " + str(result))
