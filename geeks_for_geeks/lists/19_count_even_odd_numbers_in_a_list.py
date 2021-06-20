5bcc31d53d7a45fada1b434451494892c0f73a90
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-20 11:55:30 -0500

    :art: Day 31

---
+++ geeks_for_geeks/lists/19_count_even_odd_numbers_in_a_list.py
@@ -0,0 +1,8 @@
+MY_LIST = [10, 21, 4, 45, 66, 93, 11]
+
+ONLY_ODD = len([num for num in MY_LIST if num % 2 == 1])
+ONLY_EVEN = len(MY_LIST) - ONLY_ODD
+
+if __name__ == "__main__":
+    print(f"Even numbers in the list: {ONLY_EVEN}.")
+    print(f"Odd numbers in the list: {ONLY_ODD}.")
