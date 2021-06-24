d0abce19d6a32bd6cc2e2d74759b0ac2268d9f6d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-24 16:58:24 -0500

    :sparkles: Update

--- tests/execute_random.py
+++ tests/execute_random.py
@@ -2,9 +2,9 @@ from os import popen, system
 from random import choice
 
 CURRENT_PATH = popen("pwd").read().strip()
-EDABIT = f"{CURRENT_PATH}/finished/edabit/very_easy"
-SCRIPT = choice(popen(f"ls {EDABIT}").read().split())
+DIRECTORY = f"{CURRENT_PATH}/geeks_for_geeks/lists/"
+SCRIPT = choice(popen(f"ls {DIRECTORY}*.py").read().split())
 
 if __name__ == "__main__":
     print(f"The Script to run is {SCRIPT}.")
-    system(f"python3 {EDABIT}/{SCRIPT}")
+    system(f"python3 {SCRIPT}")
