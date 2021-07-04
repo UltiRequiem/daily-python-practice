32661b1c541ea2c691faf0a073c8bc07b6fd2563
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-04 15:14:21 -0500

    :sparkles: Day 45

---
+++ geeks_for_geeks/lists/31_random_value_list_of_lists.py
@@ -0,0 +1,16 @@
+"""
+Given a list of lists. The task is to extract a random element from it.
+"""
+from random import choice
+
+
+TEST = [[4, 5, 5], [2, 7, 4], [8, 6, 3]]
+
+
+def flatten(t: list | tuple) -> list:
+    return [item for sublist in t for item in sublist]
+
+
+if __name__ == "__main__":
+    print(TEST)
+    print(choice(flatten(TEST)))
