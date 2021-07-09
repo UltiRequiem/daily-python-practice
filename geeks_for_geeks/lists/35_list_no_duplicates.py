6d5dfff774fd6d82ef60a7c2c26a7b778b1a4041
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-09 11:24:39 -0500

    :sparkles: Day 50

---
+++ geeks_for_geeks/lists/35_list_no_duplicates.py
@@ -0,0 +1,14 @@
+from functools import reduce
+
+TEST = [1, 3, 5, 6, 3, 5, 6, 1]
+# The set method clear the repeated elements
+FILTRED = set(TEST)
+
+
+def prod(lst: list | set) -> int:
+    return reduce(lambda acum, ele: acum * ele, lst)
+
+
+if __name__ == "__main__":
+    print(f"The original list is : {TEST}")
+    print(f"Duplication removal list product :{prod(FILTRED)}")
