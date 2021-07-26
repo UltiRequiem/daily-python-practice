a3ef1e54b92bf3d28dfa39242e67f25130be8a1e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-07-26 12:32:31 -0500

    :sparkles: Day 67

---
+++ python_principles/06_double_letters.py
@@ -0,0 +1,12 @@
+"""
+Your function must return True if there are two identical letters in a row in the string, and False otherwise.
+"""
+
+
+def double_letters(string: str) -> bool:
+    return any((a == b for a, b in zip(string, string[1:])))
+
+
+if __name__ == "__main__":
+    print(double_letters("hello"))
+    print(double_letters("nono"))
